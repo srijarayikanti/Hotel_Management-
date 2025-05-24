e9681f1c8b793a675b62cf1193a051e0120b9d37
Srija Rayikanti <srijarayikanti@gmail.com>
2025-05-24 22:19:34 +0530

    main

---
+++ src/main/java/com/example/Hotel_mngmt/entities/Register.java
@@ -0,0 +1,13 @@
+package com.example.Hotel_mngmt.entities;
+
+import java.io.Serializable;
+
+public class Register implements Serializable {
+    private String userName;
+    private String email;
+    private String phone;
+    private String password;
+    private String firstName;
+    private String lastName;
+    
+}
